d21400827432539be388a9df2cfd2d65cd8d02c1
sophiay2 <32136541+sophiay2@users.noreply.github.com>
2020-07-25 23:09:38 -0500

    Added homepage
    linked to sign in and register pages

---
+++ Homepage.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+<?php echo "<body style='background-color:Coral'>"; ?>
+ <?php echo '<p> <font size = 36> Welcome to Bookentory!</p>'; ?> 
+
+  <img class="mb-4" src="assets\images\frontpageimg.jpeg" alt="" width="500" height="500">
+
+<?php echo '<p>  </p>'; ?>
+
+<input type="button" value="Sign Up" onclick="location='form-register.php'" />
+<input type="button" value="Login" onclick="location='form-signin.php'" />
+ </body>
+</html>
